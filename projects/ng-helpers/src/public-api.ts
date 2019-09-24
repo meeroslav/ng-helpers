9375d2fc13fe81f6dff4b4c625388c47379698e7
Miroslav Jonas <missing.manual@gmail.com>
2019-09-24 15:34:58 +0200

    fix(api): remove missing ActionClass export

--- projects/ng-helpers/src/public-api.ts
+++ projects/ng-helpers/src/public-api.ts
@@ -7,6 +7,6 @@ export { FragmentComponent } from './lib/fragment/fragment.component';
 // NGRX HELPERS
 export { BaseAction, BaseFailedAction, BaseLoadAction, ActionClass } from './lib/ngrx/base.actions';
 export { baseReducer, ReducerHandler, ReducerHandlers } from './lib/ngrx/base.reducer';
-export { BaseEffects, ActionClass } from './lib/ngrx/base.effects';
+export { BaseEffects } from './lib/ngrx/base.effects';
 export { BaseState, baseInitialState } from './lib/ngrx/base-state';
 export { ofAction } from './lib/ngrx/ofAction';
