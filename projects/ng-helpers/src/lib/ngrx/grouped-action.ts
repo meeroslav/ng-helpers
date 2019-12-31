dcc65e765cc556dc00bb6e9541750ce9206ecbdd
Miroslav Jonas <missing.manual@gmail.com>
2019-12-31 12:12:47 +0100

    docs(ngrx): add type to grouped actions

--- projects/ng-helpers/src/lib/ngrx/grouped-action.ts
+++ projects/ng-helpers/src/lib/ngrx/grouped-action.ts
@@ -1,9 +1,17 @@
+/**
+ * @description
+ * Enum of possible action groups
+ */
 export const enum ActionGroup {
   LOAD = 'LOAD',
   SUCCESS = 'SUCCESS',
   FAILURE = 'FAILURE'
 }
 
+/**
+ * @description
+ * Type for extended action containing `actionGroup` property
+ */
 export interface GroupedAction {
   actionGroup: ActionGroup;
 }
