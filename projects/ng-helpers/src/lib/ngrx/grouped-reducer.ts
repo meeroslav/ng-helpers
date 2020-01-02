8a793852ed574ba5c9e7cce5e9d19cadd08b3203
Miroslav Jonas <missing.manual@gmail.com>
2020-01-02 12:50:39 +0100

    feat(ngrx): add documentation to grouped reducer

--- projects/ng-helpers/src/lib/ngrx/grouped-reducer.ts
+++ projects/ng-helpers/src/lib/ngrx/grouped-reducer.ts
@@ -1,7 +1,50 @@
 import { Action, ActionCreator, ActionReducer, Creator } from '@ngrx/store';
-import { ActionGroup, FailurePayload, GroupedAction, GroupedReducers, LoadingState } from './model';
+import { ActionGroup, BaseState, FailurePayload, GroupedAction, GroupedReducers, LoadingState } from './model';
 
-export function createGroupedReducer<S, A extends GroupedAction & Action>(
+/**
+ * @description
+ * Create generic reducer that automatically processes actions based on their group type
+ *
+ * @param initialState Initial/default value of the state
+ * @param creators Allowed actions that should be processed by the reducer
+ * @param config Configuration of reducer overrides for specific group of actions. See [[GroupedReducers]]
+ *
+ * @usageNotes
+ *
+ * In `actions` file:
+ * ```ts
+ * export const loadProducts = createLoadAction('[Product] Load Products');
+ * export const productsLoaded = createSuccessAction('[Product] Products Loaded', props<{ products: Product[] }>());
+ * ...
+ * ```
+ * Reducer file:
+ * ```ts
+ * import * as actions from './actions';
+ *
+ * const successReducer = createReducer(
+ *   initialState,
+ *   on(productLoaded, (state, { product }) => ({ ...state, product, loading: LoadingState.SUCCESSFUL })),
+ *   on(productsLoaded, (state, { products }) => ({ ...state, products, loading: LoadingState.SUCCESSFUL }))
+ * );
+ * export const productReducer = createGroupedReducer(initialState, actions, { successReducer });
+ * ```
+ *
+ * Which is equivalent to:
+ * ```ts
+ * import * as actions from './actions';
+ *
+ * export const productReducer = createReducer(
+ *   initialState,
+ *   on(LoadProducts, LoadProduct, ...,
+ *     (state) => ({ ...state, error: null, loading: LoadingState.LOADING })),
+ *   on(productLoaded, (state, { product }) => ({ ...state, product, loading: LoadingState.SUCCESSFUL })),
+ *   on(productsLoaded, (state, { products }) => ({ ...state, products, loading: LoadingState.SUCCESSFUL }))
+ *   on(LoadProductsFailed, LoadProductFailed, ...,
+ *     (state, { error }) => ({ ...state, error, loading: LoadingState.FAILED })),
+ * );
+ * ```
+ */
+export function createGroupedReducer<S extends BaseState, A extends GroupedAction & Action>(
   initialState: S,
   creators: { [key: string]: ActionCreator<string, Creator> },
   config?: GroupedReducers<S, A>
@@ -24,14 +67,31 @@ export function createGroupedReducer<S, A extends GroupedAction & Action>(
   };
 }
 
-function defaultLoadReducer<S>(state: S): S {
+/**
+ * Default reducer for handling all the load actions
+ * Sets loading to `LoadingState.LOADING` and resets error
+ *
+ * @param state Store state of `BaseState` type
+ */
+function defaultLoadReducer<S extends BaseState>(state: S): S {
   return { ...state, loading: LoadingState.LOADING, error: null };
 }
-
-function defaultSuccessReducer<S>(state: S): S {
+/**
+ * Default reducer for handling all the success actions
+ * Sets loading to `LoadingState.SUCCESSFUL`
+ *
+ * @param state Store state of `BaseState` type
+ */
+function defaultSuccessReducer<S extends BaseState>(state: S): S {
   return { ...state, loading: LoadingState.SUCCESSFUL };
 }
-
-function defaultFailedReducer<S, A extends FailurePayload>(state: S, action: A): S {
+/**
+ * Default reducer for handling all the failure actions
+ * Sets loading to `LoadingState.FAILED` and sets error
+ *
+ * @param state Store state of `BaseState` type
+ * @param action Action to apply to reducer. Must extend `FailurePayload`
+ */
+function defaultFailedReducer<S extends BaseState, A extends FailurePayload>(state: S, action: A): S {
   return { ...state, loading: LoadingState.FAILED, error: action.error };
 }
