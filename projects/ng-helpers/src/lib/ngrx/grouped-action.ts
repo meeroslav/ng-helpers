0a2000547a3ac4da194a5d5c263abbea9017639e
Miroslav Jonas <missing.manual@gmail.com>
2020-01-02 11:34:07 +0100

    feat(ngrx): add documentation to grouped action

--- projects/ng-helpers/src/lib/ngrx/grouped-action.ts
+++ projects/ng-helpers/src/lib/ngrx/grouped-action.ts
@@ -2,6 +2,8 @@ import { ActionCreator, Creator } from '@ngrx/store';
 import { FunctionWithParametersType, TypedAction } from '@ngrx/store/src/models';
 import { ActionGroup, FailurePayload, GroupedAction } from './model';
 
+// Abstract generic action for groped action factories
+// @param actionGroup Group of the action creator factory
 function groupedActionFactory<O extends object>(actionGroup: ActionGroup) {
   function createGroupedAction<T extends string>(
     type: T
@@ -46,6 +48,7 @@ function groupedActionFactory<O extends object>(actionGroup: ActionGroup) {
   return createGroupedAction;
 }
 
+// Extension on ngrx defineType function providing extra parameter for presetting actionGroup
 function defineGroupedType(type: string, actionGroup: ActionGroup, creator: Creator): Creator {
   return Object.defineProperties(creator, {
     type: {
@@ -58,7 +61,66 @@ function defineGroupedType(type: string, actionGroup: ActionGroup, creator: Crea
     }
   });
 }
-
+/**
+ * @description
+ * Action creator factory for LOAD actions extending action object with `actionGroup: ActionGroup.LOAD`.
+ * These actions are meant to request (load) data from external APIs or other resources.
+ *
+ * For details on usage see `ngrx` original `createAction`
+ *
+ * @usageNotes
+ *
+ * ```ts
+ * const loadProduct = createLoadAction('[Product] Load Product', props<{ id: string }>());
+ * const loadProductAction = loadProduct({ id: '12345' });
+ * ```
+ *
+ * which is equivalent to:
+ * ```ts
+ * const loadProduct = createAction('[Product] Load Product', props<{ id: string, actionGroup: ActionGroup }>());
+ * const loadProductAction = loadProduct({ id: '12345', actionGroup: ActionGroup.LOAD });
+ * ```
+ */
 export const createLoadAction = groupedActionFactory(ActionGroup.LOAD);
+/**
+ * @description
+ * Action creator factory for SUCCESS actions extending action object with `actionGroup: ActionGroup.SUCCESS`
+ * These actions are meant to store (document) data from external APIs or other resources in the state.
+ *
+ * For details on usage see `ngrx` original `createAction`
+ *
+ * @usageNotes
+ *
+ * ```ts
+ * const productLoaded = createSuccessAction('[Product] Product Loaded', props<Product>());
+ * const productLoadedAction = productLoaded(response);
+ * ```
+ *
+ * which is equivalent to:
+ * ```ts
+ * const productLoaded = createAction('[Product] Product Loaded', props<Product & { actionGroup: ActionGroup }>());
+ * const productLoadedAction = productLoaded({ ...response, actionGroup: ActionGroup.LOAD });
+ * ```
+ */
 export const createSuccessAction = groupedActionFactory(ActionGroup.SUCCESS);
+/**
+ * @description
+ * Action creator factory for FAILURE actions extending action object with `actionGroup: ActionGroup.FAILURE`
+ * These actions are meant to store (document) error from external APIs or other resources in the state.
+ *
+ * For details on usage see `ngrx` original `createAction`
+ *
+ * @usageNotes
+ *
+ * ```ts
+ * const productLoadFailed = createFailureAction('[Product] Load Product Failed', props<{ id: string }>());
+ * const productFailedAction = productLoadFailed({ id: '12345', error: responseError });
+ * ```
+ *
+ * which is equivalent to:
+ * ```ts
+ * const productLoadFailed = createAction('[Product] Load Product Failed', props<{ id: string, actionGroup: ActionGroup, error: Error }>());
+ * const productFailedAction = productLoadFailed({ id: '12345', error: responseError , actionGroup: ActionGroup.LOAD });
+ * ```
+ */
 export const createFailureAction = groupedActionFactory<FailurePayload>(ActionGroup.FAILURE);
