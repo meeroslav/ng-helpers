bf0491b512167c15cbd08a6884473a06c7e5449a
Miroslav Jonas <missing.manual@gmail.com>
2020-01-09 13:44:19 +0100

    feat(ngrx): set allowed types and first parameter in effect helpers

--- projects/ng-helpers/src/lib/ngrx/effects-helper.ts
+++ projects/ng-helpers/src/lib/ngrx/effects-helper.ts
@@ -3,7 +3,7 @@ import { Action, ActionCreator, Creator } from '@ngrx/store';
 import { Observable, of, OperatorFunction, pipe } from 'rxjs';
 import { catchError, concatMap, map, switchMap } from 'rxjs/operators';
 
-type AllowedType = string | ActionCreator<string, Creator>;
+type TypeOrCreator = string | ActionCreator<string, Creator>;
 
 /**
  * @description
@@ -21,19 +21,57 @@ export abstract class EffectsHelper {
    * @description
    * Create side effect by mapping action to a function call using `switchMap` to handle concurrency.
    *
-   * @param method Function to call with action payload
+   * @param allowedTypes Action, type or array of actions or types to filter by
+   * @param method Function to call with action payload, returns Observable
    * @param innerPipe Pipe to handle successful response and/or source
    * @param failureTarget Action to dispatch on failure
-   * @param allowedTypes Actions (or types) to filter by
+   *
+   * @usageNotes
+   * ```ts
+   * efx1$ = this.createSwitchMapEffect(
+   *   loadProducts,
+   *   this.service.getProducts,
+   *   () => map(response => productsLoaded(response)),
+   *   productsLoadFailed
+   * );
+   * efx2$ = this.createSwitchMapEffect(
+   *   [loadProduct, loadSelectedProduct]
+   *   this.service.getProduct,
+   *   () => map(response => productLoaded(response)),
+   *   productLoadFailed
+   * );
+   * ```
+   *
+   * which is equivalent to:
+   * ```ts
+   * efx1$ = createEffect(() => this.actions$.pipe(
+   *   ofType(loadProducts),
+   *   switchMap(action => this.service.getProducts(action)
+   *     .pipe(
+   *       map(response => productsLoaded(response)),
+   *       catchError(error => productsLoadFailed({ ...action, error })
+   *      )
+   *    )
+   * ));
+   * efx2$ = createEffect(() => this.actions$.pipe(
+   *   ofType(loadProduct, loadSelectedProduct),
+   *   switchMap(action => this.service.getProduct(action)
+   *     .pipe(
+   *       map(response => productLoaded(response)),
+   *       catchError(error => productLoadFailed({ ...action, error })
+   *      )
+   *    )
+   * ));
+   * ```
    */
   protected createSwitchMapEffect<REQ extends object = {}, RES = any>(
-    method: (payload: REQ) => Observable<RES>,
+    allowedTypes: TypeOrCreator | [TypeOrCreator, ...Array<TypeOrCreator>],
+    method: (action: REQ) => Observable<RES>,
     innerPipe: (action: REQ) => OperatorFunction<RES, Action>,
-    failureTarget: ActionCreator<string, Creator>,
-    ...allowedTypes: AllowedType[]
+    failureTarget: ActionCreator<string, Creator>
   ) {
     return createEffect(() => this.actions$.pipe(
-      ofType(...allowedTypes),
+      ofType(...Array.isArray(allowedTypes) ? allowedTypes : [allowedTypes]),
       switchMap((action: REQ & Action) => method(action)
         .pipe(
           this.mapInnerPipe<REQ & Action, RES>(innerPipe, failureTarget, action)
@@ -47,19 +85,57 @@ export abstract class EffectsHelper {
    * @description
    * Create side effect by mapping action to a function call using `switchMap` to handle concurrency.
    *
-   * @param method Function to call with action payload
+   * @param allowedTypes Action, type or array of actions or types to filter by
+   * @param method Function to call with action payload, returns Observable
    * @param innerPipe Pipe to handle successful response and/or source
    * @param failureTarget Action to dispatch on failure
-   * @param allowedTypes Actions (or types) to filter by
+   *
+   * @usageNotes
+   * ```ts
+   * efx1$ = this.createConcatMapEffect(
+   *   saveProducts,
+   *   this.service.saveProducts,
+   *   () => map(response => productsSaved(response)),
+   *   productsSaveFailed
+   * );
+   * efx2$ = this.createConcatMapEffect(
+   *   [deleteProduct, deleteSelectedProduct]
+   *   this.service.deleteProduct,
+   *   () => map(response => productDeleted(response)),
+   *   productDeleteFailed
+   * );
+   * ```
+   *
+   * which is equivalent to:
+   * ```ts
+   * efx1$ = createEffect(() => this.actions$.pipe(
+   *   ofType(saveProducts),
+   *   concatMap(action => this.service.saveProducts(action)
+   *     .pipe(
+   *       map(response => productsSaved(response)),
+   *       catchError(error => productsSaveFailed({ ...action, error })
+   *      )
+   *    )
+   * ));
+   * efx2$ = createEffect(() => this.actions$.pipe(
+   *   ofType(deleteProduct, deleteSelectedProduct),
+   *   concatMap(action => this.service.deleteProduct(action)
+   *     .pipe(
+   *       map(response => productDeleted(response)),
+   *       catchError(error => productDeleteFailed({ ...action, error })
+   *      )
+   *    )
+   * ));
+   * ```
    */
   protected createConcatMapEffect<REQ extends object = {}, RES = any>(
+    allowedTypes: TypeOrCreator | [TypeOrCreator, ...Array<TypeOrCreator>],
     method: (action: REQ) => Observable<RES>,
     innerPipe: (action: REQ) => OperatorFunction<RES, Action>,
-    failureTarget: ActionCreator<string, Creator>,
-    ...allowedTypes: AllowedType[]
+    failureTarget: ActionCreator<string, Creator>
   ) {
     return createEffect(() => this.actions$.pipe(
-      ofType(...allowedTypes),
+      ofType(...Array.isArray(allowedTypes) ? allowedTypes : [allowedTypes]),
       concatMap((action: REQ & Action) => method(action)
         .pipe(
           this.mapInnerPipe<REQ & Action, RES>(innerPipe, failureTarget, action)
@@ -92,35 +168,32 @@ export abstract class EffectsHelper {
    * @description
    * Filter specific Action and map it to `targetAction` with using `mapper` to modify payload
    *
-   * @param target Action to map to
-   * @param mapper Mapper function to apply to source payload
-   * @param allowedTypes Actions (or types) to filter by
+   * @param allowedTypes Action, type or array of actions or types to filter by
+   * @param mapper Mapper function to generate new action from source action's payload
    *
    * @usageNotes
    * ```ts
-   * failureEfx$ = this.createActionEffect(
-   *   ShowFailureMessage,
-   *   ({ productId }) => `Loading product ${productId} failed`,
-   *   LoadProductFailed);
+   * failureEfx$ = this.createMapEffect(
+   *   loadProductFailed,
+   *   ({ productId }) => showFailureMessage({ message: `Loading product ${productId} failed` })
+   * );
    * ```
    *
    * which is equivalent to
    * ```ts
    * failureEfx$ = createEffect(() => this.actions$.pipe(
-   *   ofAction(LoadProductFailed),
-   *   map(({ productId }) => new ShowFailureMessage(`Loading product ${productId} failed`))
+   *   ofType(loadProductFailed),
+   *   map(({ productId }) => showFailureMessage({ message: `Loading product ${productId} failed` }))
    * ));
    * ```
    */
-  protected createActionEffect<T>(
-    target: ActionCreator<string, Creator>,
-    mapper: (action: T & Action) => T,
-    ...allowedTypes: AllowedType[]
+  protected createMapEffect<T>(
+    allowedTypes: TypeOrCreator | [TypeOrCreator, ...Array<TypeOrCreator>],
+    mapper: (action: T & Action) => Action,
   ) {
     return createEffect(() => this.actions$.pipe(
-      ofType(...allowedTypes),
-      map((action: T & Action) => mapper ? mapper(action) : action),
-      map((payload: T) => target(payload) as Action) // TODO: remove `as Action` after PR
+      ofType(...Array.isArray(allowedTypes) ? allowedTypes : [allowedTypes]),
+      map((action: T & Action) => mapper(action)) // TODO: remove `as Action` after PR
     ));
   }
 }
